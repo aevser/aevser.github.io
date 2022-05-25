c2fd7189d91675714118e7e70cd51f0450f0fd1e
aevser <96634437+aevser@users.noreply.github.com>
2022-05-25 22:19:57 +0400

    Update script.js

--- calc/js/script.js
+++ calc/js/script.js
@@ -1,190 +1,88 @@
-*{
-    box-sizing: border-box;
-}
+$(document).ready(function() {
+    function formatState (state) {
+        if (!state.id) {
+          return state.text;
+        }
+      
+        var baseUrl = "../images";
+        var $state = $(
+          '<span class="flex"><img class="img-flag" /> <span></span></span>'
+        );
+      
+        // Use .text() instead of HTML string concatenation to avoid script injection issues
+        $state.find("span").text(state.text);
+        $state.find("img").attr("src", baseUrl + "/" + state.element.value.toLowerCase() + ".png");
+      
+        return $state;
+      };
+      
+      $(".configurator__block-form-select").select2({
+        templateResult: formatState,
+        templateSelection: formatState,
+        minimumResultsForSearch: Infinity
+      });
+  
+    let obj = {
+        width: $("#width").val(),
+        height: $("#height").val(),
+        form: $("#form").find(':selected').data('cof'),
+        type: $("#type").find(':selected').data('cof'),
+        quantity: $("#quantity").val(),
+    }
 
-body{
-    font-family: sans-serif;
-    background: #fbfafa;
-}
+    $("#width, #height").keyup(function() {
+        obj = changeVal(obj);
+        result(obj)
+    });
 
-.wrapper{
-    max-width: 1000px;
-    width: 100%;
-    margin: 0 auto;
-    padding: 0 15px;
-}
+    $("#type, #form").change(function() {
+        obj = changeVal(obj);
+        result(obj)
+    });
 
-.configurator__label{
-    background: #617188;
-    color: #fff;
-    font-size: 22px;
-    text-align: center;
-    padding: 15px 0;
-    width: 100%;
-}
+    $(".configurator__block-form-quantity-minus").click(function() {
+        let block = $(this).parent(".configurator__block-form-quantity");
+        let quantity = $(block).children(".configurator__block-form-input");
+        $(quantity).val( Number($(quantity).val()) - 1);
+        obj = changeVal(obj);
+        result(obj)
+    });
 
-.configurator__title{
-    color: #617188;
-    text-transform: uppercase;
-    font-size: 30px;
-    text-align: center;
-    padding: 30px 0;
-    font-weight: 300;
-    border-bottom: 1px solid #617188;
-    width: 100%;
-}
+    $(".configurator__block-form-quantity-plus").click(function() {
+        let block = $(this).parent(".configurator__block-form-quantity");
+        let quantity = $(block).children(".configurator__block-form-input");
+        $(quantity).val( Number($(quantity).val()) + 1);
+        obj = changeVal(obj);
+        result(obj)
+    });
 
-.configurator__form{
-    padding: 50px 0;
-}
+    $(".configurator__block-form-close").click(function() {
+        $(this).parent(".configurator__form-inner").remove()
+    });
 
-.configurator__form-inner{
-    display: flex;
-    margin: 0 -8px;
-    /* justify-content: space-between; */
-}
+    $(".question").click(function() {
+        $(".answer").hide();
+        $(this).parent(".configurator__block-form-title").children(".answer").toggle();
+        $(".overlay").show();
+    });
+    $(".overlay").click(function() {
+        $(".answer").hide();
+        $(".overlay").hide();
 
-.configurator__form-block{
-    /* width: 16.666666%; */
-    /* width: 20%; */
-    width: 18.3%;
-    padding: 0 8px 16px;
-}
-.configurator__form-block:nth-child(2),
-.configurator__form-block:nth-child(3){
-    width: 20%;
-}
+    });
+    function changeVal(obj) {
+        obj = {
+            width: $("#width").val(),
+            height: $("#height").val(),
+            form: $("#form").find(':selected').data('cof'),
+            type: $("#type").find(':selected').data('cof'),
+            quantity: $("#quantity").val(),
+        }
 
-.configurator__block-form-title{
-    font-size: 14px;
-    text-align: center;
-    color: #617188;
-    padding-bottom: 20px;
-    display: flex;
-    align-items: center;
-    position: relative;
-}
-.question{
-    position: relative;
-    top: 3px;
-    margin-left: 5px;
-    cursor: pointer;
-}
-.answer{
-    position: absolute;
-    bottom:  50px;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 400px;
-    background: #585757;
-    border-radius: 5px;
-    padding: 15px;
-    color: #fff;
-    display: none;
-    text-align: left;
-}
-.configurator__block-form-input{
-    padding: 0 15px;
-    font-size: 14px;
-    width: 100%;
-    height: 40px;
-    background: transparent;
-    border: 1px solid #aaa;
-}
+        return obj;
+    }
 
-.configurator__block-form-select{
-    height: 40px;
-}
-
-.configurator__block-form-quantity{
-    position: relative;
-}
-
-.configurator__block-form-quantity .configurator__block-form-input{
-    text-align: center;
-    font-size: 20px;
-    font-weight: 400;
-}
-
-.configurator__block-form-quantity-minus,
-.configurator__block-form-quantity-plus{
-    position: absolute;
-    top: 50%;
-    left: 15px;
-    transform: translateY(-50%);
-    cursor: pointer;
-}
-
-.configurator__block-form-quantity-plus{
-    left: inherit;
-    right: 15px;
-}
-
-.configurator__block-form-result{
-    text-transform: uppercase;
-    justify-content: center;
-    font-size: 20px;
-    font-weight: 400;
-    height: 40px;
-    display: flex;
-    align-items: center;
-}
-
-.configurator__block-form-close{
-    margin-top: 25px;
-    width: 17px;
-    height: 17px;
-    cursor: pointer;
-    position: relative;
-}
-
-.configurator__block-form-close::before, 
-.configurator__block-form-close::after{
-    content: "";
-    width: 20px;
-    height: 1px;
-    background-color: #617188;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50% , -50%) rotate(45deg);
-}
-
-.configurator__block-form-close::after{
-    transform: translate(-50% , -50%) rotate(135deg);
-}
-.select2{
-    width: 100% !important;
-}
-.flex{
-    display: flex;
-    align-items: center;
-}
-.flex img{
-    padding-right: 5px;
-}
-.select2-container--default .select2-selection--single{
-    height: 40px !important;
-    font-size: 14px;
-    width: 100%;
-    background: transparent;
-}
-.select2-container--default .select2-selection--single{
-    background-color: transparent !important;
-    border-radius: 0 !important;
-}
-.select2-container .select2-selection--single .select2-selection__rendered{
-    padding-top: 5px;
-}
-.select2-container--default .select2-selection--single .select2-selection__arrow b{
-    top: 75% !important;
-}
-.overlay{
-    position: fixed;
-    width: 100vw;
-    height: 100vh;
-    top: 0;
-    left: 0;
-    /* background: red */
-    display: none;
-}
+    function result(obj){
+        $("#result").text(((obj.width * obj.height)/1000) * 1.5 * obj.form * obj.type * obj.quantity)
+    }
+});
